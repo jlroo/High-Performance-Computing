2cfd26b0de51b82be4763331f099f2e9f3526e91
jose Luis Rodriguez <jrodriguezorjuela@luc.edu>
2017-11-30 12:36:09 -0600

    benchmarks with standard deviation

--- projects/03-mpi/02-code/mpi_nbody3.d
+++
@@ -1,6 +0,0 @@
-mpi_nbody3.o: mpi_nbody3.cpp ../../common/my_timer.h \
- ../../common/aligned_allocator.h ../../common/my_mpi_header.h \
- /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpi.h \
- /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpio.h \
- /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpi.h \
- /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpicxx.h
