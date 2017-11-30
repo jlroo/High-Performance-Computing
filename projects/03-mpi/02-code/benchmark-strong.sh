d978de24cec71bc603ad14cbc85edeb5d0c1ccff
jose Luis Rodriguez <jrodriguezorjuela@luc.edu>
2017-11-30 11:13:14 -0600

    added benchmars bash scripts

---
+++ projects/03-mpi/02-code/benchmark-strong.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#----------------------------------------------------
+#SBATCH -J myjob           # Job name
+#SBATCH -o myjob.o%j       # Name of stdout output file
+#SBATCH -e myjob.e%j       # Name of stderr error file
+#SBATCH -p normal          # Queue (partition) name
+#SBATCH -N 4               # Total # of nodes
+#SBATCH -n 32              # Total # of mpi tasks
+#SBATCH -t 01:30:00        # Run time (hh:mm:ss)
+
+# Launch MPI code...
+
+for i in 1 2 4 8 16 32 64 128 256;
+    do for n in 1000 2000 4000 8000 16000;
+        do ibrun -n $i ./mpi_nbody3 -n $n; 2>/dev/null;
+    done > $i-strong-output.log;
+done;
--- projects/03-mpi/02-code/benchmark-weak.sh
+++ projects/03-mpi/02-code/benchmark-weak.sh
@@ -11,7 +11,9 @@
 # Launch MPI code...
 
 
-for i in 1 2 4 8 16 32 64 128 256;do for t in 1 2 4 8 16 32 64 128 256;do n=$( printf "%.0f" $(echo "sqrt ( $t ) * 1000" | bc -l) );ibrun -n $i ./mpi_nbody3 -n $n 2>/dev/null; done > $i-weak-output.log; done;
-
-
-#for i in 64 128 256;do for n in 1000 2000 4000 8000 16000;do ibrun -n $i ./mpi_nbody3 -n $n; 2>/dev/null; done > $i-strong-output.log; done;
+for i in 1 2 4 8 16 32 64 128 256;
+    do for t in 1 2 4 8 16 32 64 128 256;
+        do n=$( printf "%.0f" $(echo "sqrt ( $t ) * 1000" | bc -l) );
+        ibrun -n $i ./mpi_nbody3 -n $n 2>/dev/null;
+    done > $i-weak-output.log;
+done;
