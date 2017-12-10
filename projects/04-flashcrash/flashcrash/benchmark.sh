d1d1b52ade2bbaa7628c1394185eb874e02eca99
jose Luis Rodriguez <jrodriguezorjuela@luc.edu>
2017-12-10 12:55:24 -0600

    sbatch added

--- projects/04-flashcrash/flashcrash/benchmark.sh
+++ projects/04-flashcrash/flashcrash/benchmark.sh
@@ -9,6 +9,7 @@
 #SBATCH -t 01:30:00        # Run time (hh:mm:ss)
 
 # Launch OpenMP code benchamark...
+# sbatch -J volume -p normal -N 1 -n 68 -t 05:00:00 benchmark.sh
 
 
 for t in 1 2 4 6 8 16 32 64; do export OMP_NUM_THREADS=$t; for i in $(ls $WORK/data/05/);do $HOME/fixanalyzer -p $WORK/data/05/$i -t 52 -n 4 -m 8 > $HOME/output/$t-${i:11:17}.log; done; done
